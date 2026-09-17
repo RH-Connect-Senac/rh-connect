14c507d221f53ffde63fc2e97d874b2a64326b54
Lucas Gabriel <136533357+LucasGabriel012@users.noreply.github.com>
2026-09-17 11:12:27 -0300

    Add external resources query DTO

---
+++ apps/api/src/external-resources/dto/list-external-resources-query.dto.ts
@@ -0,0 +1,19 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
+
+export class ListExternalResourcesQueryDto {
+  @IsOptional()
+  @IsString()
+  area?: string;
+
+  @IsOptional()
+  @IsString()
+  type?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(24)
+  limit?: number;
+}
