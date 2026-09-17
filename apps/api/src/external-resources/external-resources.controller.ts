834af8749ee384a0bcb17d0ab0a60c8838e6dc55
Lucas Gabriel <136533357+LucasGabriel012@users.noreply.github.com>
2026-09-17 14:50:54 -0300

    Allow public read for external resources

--- apps/api/src/external-resources/external-resources.controller.ts
+++ apps/api/src/external-resources/external-resources.controller.ts
@@ -1,13 +1,8 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
-import { Roles } from '../auth/decorators/roles.decorator';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { RolesGuard } from '../auth/guards/roles.guard';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ListExternalResourcesQueryDto } from './dto/list-external-resources-query.dto';
 import { ExternalResourcesService } from './external-resources.service';
 
 @Controller('candidate/materials/external-resources')
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles('CANDIDATE')
 export class ExternalResourcesController {
   constructor(private readonly externalResourcesService: ExternalResourcesService) {}
 
