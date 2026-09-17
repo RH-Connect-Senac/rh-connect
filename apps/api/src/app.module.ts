f618316869cb8e536cdfab02d19d5eb598c403e0
Lucas Gabriel <136533357+LucasGabriel012@users.noreply.github.com>
2026-09-17 11:13:38 -0300

    Register external resources module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ExternalResourcesModule } from './external-resources/external-resources.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    ExternalResourcesModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
